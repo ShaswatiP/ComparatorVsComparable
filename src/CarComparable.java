63c8b8af52461c09e9c939b2d62e63f701e47c2e
shpradhan <shpradhan@paypal.com>
2024-09-07 21:01:52 +0530

    fix spaces

--- src/CarComparable.java
+++ src/CarComparable.java
@@ -43,12 +43,12 @@ public class CarComparable {
 
         morecars.stream().forEach(anotherCar -> System.out.println(anotherCar.toString()));
         /* also based on above definition of AnotherCar class that implements Comparable interface, We can only sort in ascending order of CarType
-        *to sort based on descending order of CarType, we have to change the compareTo() as below.
+        * to sort based on descending order of CarType, we have to change the compareTo() as below.
         *
-        @Override
-        public int compareTo(AnotherCar other) {
-            return other.carType.compareTo(this.carType);
-        }
+                @Override
+                public int compareTo(AnotherCar other) {
+                    return other.carType.compareTo(this.carType);
+                }
         *
         * above issue doesn't happen in comparator with Lambda Expression:
         *   Collections.sort(cars,(Car c1, Car c2) -> c2.carType.compareTo(c1.carType));
